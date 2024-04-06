27070d68da7d1aa38dcc1fd49a0290d83022a3e2
Kim Nguyen <thuyduongvnguyen@gmail.com>
2024-04-06 11:28:20 -0500

    Base Code for Webpage
    Base HTML, CSS, and JS Code

---
+++ bananazascript.js
@@ -0,0 +1,4 @@
+// Example Use: A script that scrolls to a specific section when a button is clicked
+document.getElementById('yourButtonId').addEventListener('click', function() {
+    document.getElementById('yourSectionId').scrollIntoView();
+});
