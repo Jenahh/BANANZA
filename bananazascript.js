6a0d2b4df135b4a2daf28febb49910bd4f545503
Kim Nguyen <thuyduongvnguyen@gmail.com>
2024-04-06 20:16:16 -0500

    Updated the descriptions

--- bananazascript.js
+++
@@ -1,4 +0,0 @@
-// Example Use: A script that scrolls to a specific section when a button is clicked
-document.getElementById('yourButtonId').addEventListener('click', function() {
-    document.getElementById('yourSectionId').scrollIntoView();
-});
